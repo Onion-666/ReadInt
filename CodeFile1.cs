5d9588de763e104aed2255601a837d0bb1d86d8f
Халяпин Вадим <vadim.halyapin@yandex.ru>
2020-09-21 12:35:38 +0300

    Добавьте файлы проекта.

---
+++ CodeFile1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+
+class ReadInt
+{
+    static void Main()
+    {
+        string res, txt;
+        int year = 2020, age, born;
+        res = Interaction.InputBox("В каком году вы родились?", "Год рождения");
+        born = Int32.Parse(res);
+        age = year - born;
+        txt = "Вам " + age + " лет!";
+        MessageBox.Show(txt, "Возраст");
+    }
+}
